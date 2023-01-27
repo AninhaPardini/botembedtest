091f355e736e02e5f54f0f7d16d6a0d7c8b0a7ab
Aninha Pardini <aninhapardini.contato@gmail.com>
2023-01-27 14:23:01 -0300

    feat: remove old mg cities list

--- index.js
+++ index.js
@@ -238,36 +238,6 @@ bot.on(Events.InteractionCreate, async (interaction) => {
       `A área da cidade ${state.label} foi adicionada para você!`
     );
   } else if (
-    isButton &&
-    interaction.customId === INTERACTION_IDS.MG_REGION_BUTTON
-  ) {
-    // interação quando é escolhido minas gerais
-    const mgEmbed = new EmbedBuilder()
-      .setColor(0x2f3136)
-      .setTitle(':earth_americas: Escolha sua Região')
-      .setDescription(
-        'Então você mora na raiz do nosso time! Selecione abaixo se houver a opção da sua cidade. Se não houver a sua cidade, não se preocupe! É só enviar uma mensagem pedindo para adicionar sua cidade no <#1040370984613584959> que um dos moderadores irá criar para você!'
-      );
-
-    const components = new ActionRowBuilder().setComponents(
-      new StringSelectMenuBuilder()
-        .setPlaceholder('Selecione sua cidade')
-        .setCustomId(INTERACTION_IDS.CITIES_SELECT_MENU)
-        .addOptions(
-          mgCitiesOptions.map((cityOptions) => ({
-            label: cityOptions.label,
-            description: cityOptions.description,
-            value: cityOptions.value,
-          }))
-        )
-    );
-
-    interaction.update({
-      ephemeral: true,
-      embeds: [mgEmbed],
-      components: [components],
-    });
-  } else if (
     interaction.isStringSelectMenu() &&
     interaction.customId === INTERACTION_IDS.CITIES_SELECT_MENU
   ) {
